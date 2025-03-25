69a2a8a520aa61d8bfe3ae554b9ae314bb4dfc66
kbim2363 <bernikiss13@gmail.com>
2025-03-25 13:35:48 +0200

    berni

--- server_files/database_op.py
+++ server_files/database_op.py
@@ -1,11 +1,28 @@
 import os
 import json
 
-# Az adatbázisok tárolása JSON fájlban
+current_database = None  # Globális változó az aktuális adatbázishoz
+current_db_metadata = None
+
+#adatbazis tarolasa json faljba
 DB_FILE = "databases.json"
+BASE_DIR = "databases"
+
+def use_database(db_name):
+    """Beállítja az aktuális adatbázist és betölti az adatbázis JSON fájlját"""
+    global current_database, current_db_metadata
+    db_path = os.path.join(BASE_DIR, db_name)
+    db_meta_path = os.path.join(db_path, "database.json")  # Metaadatokat tároló JSON fájl
+
+    if not os.path.exists(db_path) or not os.path.exists(db_meta_path):
+        return 1  # Az adatbázis vagy annak metaadata nem létezik
+
+    # Beállítjuk az aktuális adatbázist
+    current_database = db_name
+
 
 def load_databases():
-    """Betölti az adatbázisok listáját a JSON fájlból."""
+    #betolti az adatrbazist
     if os.path.exists(DB_FILE):
         with open(DB_FILE, "r") as f:
             return json.load(f)
@@ -13,11 +30,12 @@ def load_databases():
         return {}
 
 def save_databases(databases):
-    """Ment egy adatbázist a JSON fájlba."""
     with open(DB_FILE, "w") as f:
         json.dump(databases, f, indent=4)
 
 def create_database(db_name):
+    #letrehoz egy uj foldert minden adatbazisnak
+    db_path = os.path.join(BASE_DIR, db_name)
     """Létrehoz egy új adatbázist a JSON fájlban"""
     # Betöltjük a jelenlegi adatbázisokat
     databases = load_databases()
@@ -25,10 +43,10 @@ def create_database(db_name):
     # Ellenőrizzük, hogy már létezik-e az adatbázis
     if db_name in databases:
         return 1  # Ha létezik már, nem hozhatjuk létre újra
-    
+
     # Hozzáadjuk az új adatbázist
     databases[db_name] = {"tables": {}}
-    
+
     # Mentjük a változásokat
     save_databases(databases)
     return 0  # Sikeresen létrehoztuk az adatbázist
